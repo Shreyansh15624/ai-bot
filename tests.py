3d547ece6546012b0571911d00269e57d71b05c2
Shreyansh15624 <shreyanshvajroju@gmail.com>
2025-10-28 20:19:33 +0000

    Finished Ch-2: L5

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_files_content import get_files_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 """
 print(get_files_info("calculator", "."))
@@ -27,10 +28,26 @@ print("-------------------------------------------------------------")
 """
 
 
-
+"""
 print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 print("-------------------------------------------------------------")
 print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 print("-------------------------------------------------------------")
 print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+print("-------------------------------------------------------------")
+"""
+
+
+
+print(run_python_file("calculator", "main.py"))
+print("-------------------------------------------------------------")
+print(run_python_file("calculator", "main.py", ["3 + 5"]))
+print("-------------------------------------------------------------")
+print(run_python_file("calculator", "tests.py"))
+print("-------------------------------------------------------------")
+print(run_python_file("calculator", "../main.py"))
+print("-------------------------------------------------------------")
+print(run_python_file("calculator", "nonexistent.py"))
+print("-------------------------------------------------------------")
+print(run_python_file("calculator", "lorem.txt"))
 print("-------------------------------------------------------------")
